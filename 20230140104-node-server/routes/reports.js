c00d7d3e7c34e6dab357de3b718a6315c76f19d6
Azkalyasin <yasinadzkiaazkal@gmail.com>
2025-10-30 13:02:02 +0700

    Add praktikum pertemuan 5

--- 20230140104-node-server/routes/reports.js
+++ 20230140104-node-server/routes/reports.js
@@ -5,4 +5,5 @@ const { addUserData, isAdmin } = require('../Middleware/promissionMiddleware');
 
 
 router.get('/daily', [addUserData, isAdmin], reportController.getDailyReport);
+router.get('/by-date', [addUserData, isAdmin], reportController.getDailyReportByDate)
 module.exports = router;
