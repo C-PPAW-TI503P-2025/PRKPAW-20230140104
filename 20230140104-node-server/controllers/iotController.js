eff79c4f67cd79766f8a3bb25a5f5f7b3a359830
Azkalyasin <yasinadzkiaazkal@gmail.com>
2026-01-05 20:09:59 +0700

    add code praktikum12

--- 20230140104-node-server/controllers/iotController.js
+++ 20230140104-node-server/controllers/iotController.js
@@ -4,3 +4,8 @@ exports.testConnection = (req, res) => {
   res.status(200).json({ status: "ok", reply: "Server menerima koneksi!" });
 };
 
+exports.receiveSensorData = (req, res) => {
+  const { suhu, kelembaban, cahaya } = req.body;
+  console.log(`🔥 [LOG] Suhu: ${suhu}°C | Lembab: ${kelembaban}% | Cahaya: ${cahaya}`);
+  res.status(200).json({ status: "diterima" });
+};
--- 20230140104-node-server/routes/iot.js
+++ 20230140104-node-server/routes/iot.js
@@ -3,4 +3,5 @@ const router = express.Router();
 const iotController = require('../controllers/iotController');
 
 router.post('/ping', iotController.testConnection);
+router.post('/data', iotController.receiveSensorData);
 module.exports = router;
